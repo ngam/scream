fb51ad7c4c5516170991cf78efae6da81e1d63b3
Aaron Donahue <donahue.aaron@gmail.com>
2021-09-20 16:04:26 -0700

    Add a spa+rrtmgp coupled test to the test suite.
    This also involved fixing a few mistakes in the spa interface that did
    not show up until we attempted to couple SPA with another process.

---
+++ components/scream/tests/coupled/spa_rrtmgp/spa_rrtmgp.cpp
@@ -0,0 +1,74 @@
+#include <catch2/catch.hpp>
+
+// Boiler plate, needed for all runs
+#include "control/atmosphere_driver.hpp"
+#include "share/atm_process/atmosphere_process.hpp"
+// Boiler plate, needed for when physics is part of the run
+#include "physics/share/physics_only_grids_manager.hpp"
+#include "physics/spa/atmosphere_prescribed_aerosol.hpp"
+#include "physics/rrtmgp/atmosphere_radiation.hpp"
+// EKAT headers
+#include "ekat/ekat_pack.hpp"
+#include "ekat/ekat_parse_yaml_file.hpp"
+
+namespace scream {
+
+TEST_CASE("spa-rrtmgp", "") {
+  using namespace scream;
+  using namespace scream::control;
+
+  constexpr int num_iters = 4;
+  constexpr Real dt = 864000;
+
+  // Load ad parameter list
+  std::string fname = "input.yaml";
+  ekat::ParameterList ad_params("Atmosphere Driver");
+  REQUIRE_NOTHROW ( parse_yaml_file(fname,ad_params) );
+
+  // Create a comm
+  ekat::Comm atm_comm (MPI_COMM_WORLD);
+
+  // Initialize yakl
+  if(!yakl::isInitialized()) { yakl::init(); }
+
+
+  // Need to register products in the factory *before* we create any atm process or grids manager.
+  auto& proc_factory = AtmosphereProcessFactory::instance();
+  proc_factory.register_product("SPA",&create_atmosphere_process<SPA>);
+  proc_factory.register_product("RRTMGP",&create_atmosphere_process<RRTMGPRadiation>);
+  auto& gm_factory = GridsManagerFactory::instance();
+  gm_factory.register_product("Physics Only",&physics::create_physics_only_grids_manager);
+
+  // Create the grids manager
+  auto& gm_params = ad_params.sublist("Grids Manager");
+  const std::string& gm_type = gm_params.get<std::string>("Type");
+  auto gm = GridsManagerFactory::instance().create(gm_type,atm_comm,gm_params);
+
+  // Create the driver
+  AtmosphereDriver ad;
+
+  // Init and run
+  util::TimeStamp time (0,0,0,0);
+
+  printf("Initialization...");
+  ad.initialize(atm_comm,ad_params,time);
+  printf(" done\n");
+
+  // Run the code
+  for (int i=0; i<num_iters; ++i) {
+    if (i % 10 == 0) {
+      printf("  -  %5.2f%%\nRun iteration: %d, ",(Real)i/Real(num_iters)*100,i+1);
+    } else {
+      printf("%d, ",i);
+    }
+    ad.run(dt);
+  }
+
+  // Finalize
+  printf("Finalization ...");
+  ad.finalize();
+  printf(" done\n");
+
+}
+
+} // empty namespace
