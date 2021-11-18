ea123cec95af6755ced872f8b12c9c8eea674014
Luca Bertagna <lbertag@sandia.gov>
2021-11-18 00:32:41 -0700

    bkp

--- components/scream/src/dynamics/homme/atmosphere_dynamics.hpp
+++ components/scream/src/dynamics/homme/atmosphere_dynamics.hpp
@@ -70,8 +70,7 @@ protected:
   void update_pressure ();
 
   // Copy initial states from n0 timelevel to other timelevels
-  void copy_dyn_states_to_all_timelevels (const bool compute_theta_from_T);
-  // void store_prev_fields (const view_Nd_type<3>& uv, const view_Nd_type<2>& w);
+  void copy_dyn_states_to_all_timelevels ();
 
   void initialize_impl (const RunType run_type);
 protected:
